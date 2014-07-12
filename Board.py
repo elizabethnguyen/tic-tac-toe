b768586b993dfb52d9c8ccf67e37c6c92eab32e4
Liz <Liz@liznguyen.net>
2014-07-12 16:47:47 -0700

    More cleanup.

--- Board.py
+++ Board.py
@@ -4,7 +4,7 @@ class Board:
     # Initialize the board with a list to keep track of moves
     # Not indexing by 0 to make the layout 'simple'.
     def __init__(self):
-        Board.tile = [0] * 10    
+        Board.tile = [None] * 10    
 
         # Victory Message Turtle
         Board.message = turtle.Turtle()
@@ -12,50 +12,60 @@ class Board:
         Board.message.up()
         Board.message.goto(-50,-130)
 
-    def check_victory(self, playerNum):
+    def check_victory(self, player):
+        if Board.tile[1] == player:
+            if Board.tile[5] == player:
+                if Board.tile[9] == player:
+                    Board.victory(self,player)
+                    return True
+            if Board.tile[4] == player:
+                if Board.tile[7] == player:
+                    Board.victory(self,player)
+                    return True
+            if Board.tile[2] == player:
+                if Board.tile[3] == player:
+                    Board.victory(self,player)
+                    return True
+        if Board.tile[4] == player:
+            if Board.tile[5] == player:
+                if Board.tile[6] == player:
+                    Board.victory(self,player)
+                    return True
+        if Board.tile[7] == player:
+            if Board.tile[8] == player:
+                if Board.tile[9] == player:
+                    Board.victory(self,player)
+                    return True
+            if Board.tile[5] == player:
+                if Board.tile[3] == player:
+                    Board.victory(self,player)
+                    return True
+        if Board.tile[2] == player:
+            if Board.tile[5] == player:
+                if Board.tile[8] == player:
+                    Board.victory(self,player)
+                    return True
+        if Board.tile[3] == player:
+            if Board.tile[6] == player:
+                if Board.tile[9] == player:
+                    Board.victory(self,player)
+                    return True
+        return False
+
+    def check_tie(self):
         counter = 0
-        if Board.tile[1] == playerNum:
-            if Board.tile[5] == playerNum:
-                if Board.tile[9] == playerNum:
-                    return playerNum
-            if Board.tile[4] == playerNum:
-                if Board.tile[7] == playerNum:
-                    return playerNum
-            if Board.tile[2] == playerNum:
-                if Board.tile[3] == playerNum:
-                    return playerNum
-        if Board.tile[4] == playerNum:
-            if Board.tile[5] == playerNum:
-                if Board.tile[6] == playerNum:
-                    return playerNum
-        if Board.tile[7] == playerNum:
-            if Board.tile[8] == playerNum:
-                if Board.tile[9] == playerNum:
-                    return playerNum
-            if Board.tile[5] == playerNum:
-                if Board.tile[3] == playerNum:
-                    return playerNum
-        if Board.tile[2] == playerNum:
-            if Board.tile[5] == playerNum:
-                if Board.tile[8] == playerNum:
-                    return playerNum
-        if Board.tile[3] == playerNum:
-            if Board.tile[6] == playerNum:
-                if Board.tile[9] == playerNum:
-                    return playerNum
-        for i in Board.tile:
-            if Board.tile[i] != 0:
+        for i in range(0,10):
+            if Board.tile[i] is not None:
                 counter += 1
         if counter == 9:
-            return 3
-        return 0
-
-    def victory(self, player):
-        if player == 3:
             Board.message.write("Cats Game!")
             print "Cats Game! \nThe game has ended. Please click on the window to close the game."
+            return True
         else:
-            Board.message.write("%s Wins!" % player.name)
-            print "%s wins! \nThe game has ended. Please click on the window to close the game." % player.name
+            return False
+
+    def victory(self, player):
+        Board.message.write("%s Wins!" % player.name)
+        print "%s wins! \nThe game has ended. Please click on the window to close the game." % player.name
 
 
--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@ from Player import Player
 from Board import Board
 import turtle
 
-# Initialize the turtles and screen for the game.
+# Initialize the window for the game.
 wn = turtle.Screen()
 wn.screensize(339, 296)
 wn.setup(width=350, height=300, startx=140, starty=None)
@@ -14,31 +14,28 @@ p2 = Player()
 board = Board()
         
 def get_next_turn(player):
-    if player == 0:
-        return 1
-    if player == 1:
-        return 2
-    if player == 2:
-        return 1
+    if player is None:
+        return p1
+    if player == p1:
+        return p2
+    if player == p2:
+        return p1
 
 def game():
-    winner = 0
-    which_player = 0
-    while winner == 0:
-        which_player = get_next_turn(which_player)
-        move = raw_input("Player %s's move: " % which_player)
-        if which_player == 1:
-            player = p1
-        if which_player == 2:
-            player = p2
+    endGame = False
+    player = None
+
+    while endGame == False:
+        player = get_next_turn(player)
+        move = raw_input("%s's move: " % player.name)
 
         # Conditions to ensure the user does not try to break the game!
         while True:
             try:
                 int(move)
                 if int(move) < 10 and int(move) > 0:
-                    if board.tile[int(move)] == 0:
-                        board.tile[int(move)] = which_player
+                    if board.tile[int(move)] == None:
+                        board.tile[int(move)] = player
                         break
                     else:
                         move = raw_input("Please enter a valid move: ")
@@ -49,13 +46,9 @@ def game():
 
         player.move_token(int(move))
         player.draw_shape()
-        winner = board.check_victory(which_player)
-        if winner == which_player:
-            board.victory(player)
-            break       
-        if winner == 3:
-            board.victory(3)
-            break
-
+        if board.check_victory(player) is True \
+                or board.check_tie() is True:
+            endGame = True
+        
 game()
 wn.exitonclick()
